f493259366b035e6db0125a9cdb4c287f22d8b69
rjrajujha <rjrajujha.com@outlook.com>
2022-12-18 12:40:42 +0530

    wether updt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+let weatherData;
+
+// Fectch data from API;
+fetch('https://goweather.herokuapp.com/weather/Curitiba')
+  .then((val) => {
+    weatherData = val
+    return val.json();
+  })
+  .then((val2) => {
+    console.log("data fetched from github/mock_data");
+  }).catch((err) => { console.log(`Error is ${err}`) })
+
+
 
 function App() {
   return (
     <React.Fragment>
       <p>Under cunstruction</p>
+      <p>{weatherData}</p>
     </React.Fragment>
   );
 }
